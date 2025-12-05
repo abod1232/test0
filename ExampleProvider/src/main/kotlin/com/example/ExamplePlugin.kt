be10da791837ad51fc18ab8d27c9efa7517c5d28
abod1232 <abgost4@outlook.com>
2025-12-05 18:24:31 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,25 +1,11 @@
-package com.example
+package com.akwam.provider
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
-@CloudstreamPlugin
-class ExamplePlugin: Plugin() {
-    private var activity: AppCompatActivity? = null
-
-    override fun load(context: Context) {
-        activity = context as? AppCompatActivity
-
-        // All providers should be added in this manner
-        registerMainAPI(ExampleProvider())
-
-        openSettings = {
-            val frag = BlankFragment(this)
-            activity?.let {
-                frag.show(it.supportFragmentManager, "Frag")
-            }
-        }
+@Plugin
+class AkwamPlugin : CloudstreamPlugin() {
+    override fun load() {
+        registerMainAPI(Akwam())
     }
 }
