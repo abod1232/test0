4f27b7d34c63e3ec5f7eec239bc2040d68ecb0f3
abod1232 <abgost4@outlook.com>
2025-12-05 19:07:38 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -2,10 +2,14 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
-@Plugin
-class ExamplePlugin : CloudstreamPlugin() {
-    override fun load() {
-        registerMainAPI(Akwam())
+@CloudstreamPlugin
+open class akwamPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(AkwamProvider())
+
     }
+
 }
