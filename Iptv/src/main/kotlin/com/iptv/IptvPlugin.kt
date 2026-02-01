14b38f798c49e32caeebd2f995b9bb6d55022bb1
abod1232 <abgost4@outlook.com>
2026-02-01 19:05:29 +0300

    Update IptvPlugin.kt

--- Iptv/src/main/kotlin/com/iptv/IptvPlugin.kt
+++ Iptv/src/main/kotlin/com/iptv/IptvPlugin.kt
@@ -2,11 +2,22 @@ package com.iptv
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.getValue
 import com.iptv.VipTV
-
+import com.iptv.iptvSettings
+import androidx.preference.PreferenceManager
 @CloudstreamPlugin
 class eishkPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(VipTV())
+        registerMainAPI(Viptv())
+        val prefs by lazy { PreferenceManager.getDefaultSharedPreferences(context) }
+
+        openSettings = { activityContext ->
+            (activityContext as? AppCompatActivity)?.let { activity ->
+                // ببساطة قم بإنشاء وعرض شاشة الإعدادات
+                iptvSettings().show(activity.supportFragmentManager, "ReplaymatchSettings")
+            }
+        }
     }
 }
