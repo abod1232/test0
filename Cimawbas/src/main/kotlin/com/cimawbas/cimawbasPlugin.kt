cc0c7f3f9f173f5232cdc92315cb18400aca5123
abod1232 <abgost4@outlook.com>
2026-03-10 01:57:02 +0300

    Update cimawbasPlugin.kt

--- Cimawbas/src/main/kotlin/com/cimawbas/cimawbasPlugin.kt
+++ Cimawbas/src/main/kotlin/com/cimawbas/cimawbasPlugin.kt
@@ -1,4 +1,4 @@
-package com.cimawbas
+package com.lagradost.cloudstream3.plugins
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
