05e2461eff40eb518d155cff69f1a08ae8102c92
abod1232 <abgost4@outlook.com>
2026-05-31 19:54:13 +0300

    Update SettingsFragment.kt

--- Youtube/src/main/kotlin/com/youtube/SettingsFragment.kt
+++ Youtube/src/main/kotlin/com/youtube/SettingsFragment.kt
@@ -22,7 +22,7 @@ class YoutubeSettingsBottomSheet : DialogFragment() {
     private val webStack = Stack<WebView>()
 
     // ✨ تم تحديث الـ User-Agent بناءً على طلبك الأخير (أندرويد)
-    private val USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
+    private val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 
     // ✨ الهيدرات الدقيقة المستخرجة من سجلاتك
     private val customHeaders = mapOf(
